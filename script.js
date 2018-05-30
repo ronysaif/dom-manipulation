de14decc3b446cf6891975df487d0829210f0a19
Khalid Saifullah Rony <ksaifrony@gmail.com>
2018-05-30 04:25:45 +0000

    task1

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 /* Task 1 */
 
-var heading = document.getElementById('heading')
-console.log(heading.InnerText)
+var heading = document.getElementById('heading');
+console.log(heading.InnerText);
 /* Task 2 using plain JavaScript */
-
+//task 2a
+var textBin= document.getElementById('textBin');
+textBin.addEventListener('click', function(){
+    var task2 = document.getElementById('task2a');
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "Hello World";
+    task2.appendChild(paragraph);
+});
 
 
 /* Task 4 using jQuery */

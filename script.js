0b77af21615ec9dedf744df2ffdcb6a9ef502628
Khalid Saifullah Rony <ksaifrony@gmail.com>
2018-05-30 04:53:20 +0000

    task 2c

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 /* Task 1 */
+var heading = document.getElementById('heading')
+console.log(heading.innerText)
 
-var heading = document.getElementById('heading');
-console.log(heading.InnerText);
+
 /* Task 2 using plain JavaScript */
-//task 2a
-var textBin= document.getElementById('textBin');
-textBin.addEventListener('click', function(){
-    var task2 = document.getElementById('task2a');
-    var paragraph = document.createElement('p');
-    paragraph.innerText = "Hello World";
-    task2.appendChild(paragraph);
-});
+//task 2a 
+function addText2(){
+	var task2div = document.getElementById('task2a')
+	var paragraph = document.createElement('p')
+	paragraph.innerText = "Hello World"
+	task2div.appendChild(paragraph)
+}
 
+	var textBtn = document.getElementById('textBtn2a')
+	textBtn.addEventListener('click',addText2)
+	
+//Task 2b
+function changeBGcolor(event){
+	var body = document.getElementsByTagName('body')[0]
+	if(event.target.innerText == "Red"){
+		body.style.backgroundColor = 'red'
+	}
+	if(event.target.innerText == "Green"){
+		body.style.backgroundColor = 'green'
+	}		
+}
+//Task 2c
+function redirectToGoogle(){
+	window.location.assign('http://google.com')
+
+}
 
 /* Task 4 using jQuery */
